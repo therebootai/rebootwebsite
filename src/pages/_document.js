f26a643c51997fbadc0e0588d3378d747178af76
Achinta27 <achinta27sep1999@gmail.com>
2024-12-06 23:34:54 +0530

    modified

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,14 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>
-          Welcome to Reboot AI Pvt Ltd - Top Web, App Development Company
-        </title>
-        <meta
-          name="description"
-          content="IT Solutions for the Future, Most Trusted Startups for Web, Apps & Performance Marketing Services."
-        />
-
         <link rel="preload" as="video" href="/videos/header-cover.mp4" />
         <link rel="preload" as="image" href="/images/subbannerbg.webp" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
