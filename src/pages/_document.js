6e7111f7c79f442f6f19d95e32b08515a6af68c6
Achinta27 <achinta27sep1999@gmail.com>
2024-12-07 00:31:11 +0530

    modified

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,28 +9,24 @@ export default function Document() {
         <link rel="preload" as="image" href="/images/subbannerbg.webp" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-SRB8Q22ETH"
+          async
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MR673PQX');`,
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-SRB8Q22ETH');
+              `,
           }}
         />
       </Head>
       <body className="antialiased">
-        {/*  Google Tag Manager (noscript)  */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MR673PQX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript)  */}
         <Main />
         <NextScript />
       </body>
