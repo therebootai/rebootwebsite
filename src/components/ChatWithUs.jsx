0305fc26faa9767cdf3524d7421b6019c9fb2dba
Achinta27 <achinta27sep1999@gmail.com>
2024-12-16 14:23:31 +0530

    modified

--- src/components/ChatWithUs.jsx
+++ src/components/ChatWithUs.jsx
@@ -40,14 +40,14 @@ const ChatWithUs = () => {
           href="https://api.whatsapp.com/send?phone=919088576170"
           target="_blank"
           rel="noopener"
-          className="bg-[#00C16A] text-3xl lg:text-2xl  border border-white gap-2  text-white rounded-full justify-center items-center flex px-5 lg:px-6 h-[3.5rem] lg:h-[3.5rem] cursor-pointer"
+          className="bg-[#00C16A] text-2xl lg:text-2xl  border border-white gap-2  text-white rounded-full justify-center items-center flex px-4 md:px-5 lg:px-6 h-[3rem] md:h-[3.5rem] lg:h-[3.5rem] cursor-pointer"
         >
           <span className="font-semibold">
             <div>
               <FaWhatsapp />
             </div>
           </span>
-          <h1 className="lg:text-lg text-xl font-medium text-white">
+          <h1 className="md:text-lg  text-base font-medium text-white">
             Chat With Us
           </h1>
         </Link>
